c73628b16e5a9c6a518889eff85707651252d430
Deepakjangra605 <Deepakjangra605@gmail.com>
2026-07-13 05:29:48 +0530

    WIP for Checkbox state handling

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
-  const TaskTile({super.key});
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is a task.'),
-      trailing: Checkbox(value: false, onChanged: (isChecked) {}),
+      trailing: TaskCheckbox(isChecked, (newValue) {
+        setState(() {
+          isChecked = newValue;
+        });
+      }),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final ValueChanged<bool> callback;
+
+  const TaskCheckbox(this.checkboxState, this.callback);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkboxState,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: (newValue) {
+        callback(newValue!);
+      },
     );
   }
 }
